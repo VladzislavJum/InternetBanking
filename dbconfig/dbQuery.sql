45b6629ad464e9d56f8b6fa39338b253120e2471
VladzislavJum <vladzislavjum@outlook.com>
2015-10-14 18:03:14 +0200

    Add dbQuery

---
+++ dbconfig/dbQuery.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE user_ (
+id serial PRIMARY KEY,
+username varchar(20) NOT NULL,
+surname varchar(20) NOT NULL,
+secondName varchar(20) NOT NULL,
+passportNumber varchar(20) NOT NULL,
+);
